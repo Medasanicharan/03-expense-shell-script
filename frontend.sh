1599c19aed359ba00af77ea5e686c4ce38c0ef05
charan <medasanicharan66@gmail.com>
2025-02-20 15:43:25 +0530

    expense-project

---
+++ frontend.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+dnf install nginx -y &>>$LOGFILE
+VALIDATE $? "Installing nginx"
+
+systemctl enable nginx &>>$LOGFILE
+VALIDATE $? "Enabling nginx"
+
+systemctl start nginx &>>$LOGFILE
+VALIDATE $? "Starting nginx"
+
+rm -rf /usr/share/nginx/html/* &>>$LOGFILE
+VALIDATE $? "Remove existing content"
+
+curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>>$LOGFILE
+VALIDATE $? "Downloading frontend code"
+
+cd /usr/share/nginx/html &>>$LOGFILE
+VALIDATE $? "Open the html file"
+
+unzip /tmp/frontend.zip &>>$LOGFILE
+VALIDATE $? "Unzip the file"
+
+cp /home/ec2-user/02-expense-shell-script/expense.conf /etc/nginx/default.d/expense.conf &>>$LOGFILE
+VALIDATE $? "Copied frontend service"
+
+systemctl restart nginx &>>$LOGFILE
+VALIDATE $? "Restarting nginx"
---
+++ mysql.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+source ./common.sh
+
+check_root
+
+echo "please enter DB password:"
+read mysql_root_password
+
+dnf install mysql-server -y &>>$LOGFILE
+VALIDATE $? "Installing MySQL server"
+
+systemctl enable mysqld &>>$LOGFILE
+VALIDATE $? "Enabling MySQL server"
+
+systemctl start mysqld &>>$LOGFILE
+VALIDATE $? "Starting MySQL server"
+
+# mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
+# VALIDATE $? "Setting up root password"
+
+# below code will be useful for idempotent nature
+
+mysql -h db.daws78s.xyz -uroot -p${mysql_root_password} -e 'SHOW DATABASES;' &>>$LOGFILE
+if [ $? -ne 0 ]
+then
+    mysql_secure_installation --set-root-pass ${mysql_root_password}
+    VALIDATE $? "Setting up root password"
+else
+    echo -e "MySQL root password is already setup.. $Y SKIPPING $n"
+fi
