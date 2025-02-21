d26d22350112a412d59d1fc3b8a0d5a4d1b9f25d
charan <medasanicharan66@gmail.com>
2025-02-21 20:46:48 +0530

    expense-project

--- common.sh
+++ common.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-set -e
+# set -e
 
-handle_error(){
-    echo "Failed at line number: $1, line number: $2"
-}
+# handle_error(){
+#     echo "Failed at line number: $1, line number: $2"
+# }
 
-trap 'handle_error ${LINENO} "$BASH_COMMAND"' ERR
+# trap 'handle_error ${LINENO} "$BASH_COMMAND"' ERR
 
 USERID=$(id -u)
 
